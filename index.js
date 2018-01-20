41f1c3d9ca227211d079e0c32753adca8d22961c
clubmax27 <Glados.Plague@gmail.com>
2018-01-21 00:32:34 +0100

    Added comments

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ client.login("YOUR_TOKEN");
 client.on("ready", function()
 {
   var options = {type: 0}
-  client.user.setActivity("",options);
+  client.user.setActivity("",options); //setting the activity to null when the selfbot start
   console.log("Logged in");
 });
 
@@ -15,10 +15,10 @@ client.on('message', function(msg)
 {
   if(msg.content.toUpperCase().startsWith("STATECHANGER "))
   {
-    var param_phrase = msg.content.slice(15);
-    var param_type = msg.content.charAt(13);
-    var options = {type: Number(param_type)}
-    client.user.setActivity(param_phrase,options);
-    console.log("State changed to state " + param_type + " with the name of game '" + param_phrase + "'");
+    var param_phrase = msg.content.slice(15); //cut out the prefix + the 2 spaces + the type number
+    var param_type = msg.content.charAt(13);// get the 14th char (prefix + 1 space)
+    var options = {type: Number(param_type)}//Set the option for the setActivity
+    client.user.setActivity(param_phrase,options);//set the activity
+    console.log("State changed to state " + param_type + " with the name of game '" + param_phrase + "'");//Keep a trace of what you
   }
 });
