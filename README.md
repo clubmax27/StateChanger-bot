768ec8acef7b114c0ea3d43dff7ae54c28aeb5a7
clubmax27 <Glados.Plague@gmail.com>
2018-01-21 14:10:31 +0100

    Added a activity checker + changed usage
    Now usage for StateChanger is :
    StateChanger change [type] [game]
    And to check your activity :
    StateChanger status

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 A selfbot for discord for changing your presence, with discord.js<br/>
 <br/>
 Pre-Usage : Change the "YOUR_TOKEN" in index.js to your actual token<br/>
-Usage : StateChanger [Type] [name of the activity]<br/>
+Usage : StateChanger Changer [Type] [name of the activity] : Change your activity<br/>
 Type can go from 0 to 3, and means :<br/>
 0 : Playing<br/>
 1 : Streaming<br/>
 2 : Listening<br/>
 3 : watching<br/>
+<br/>
+StateChanger Status : print in the console the author's activity
--- index.js
+++ index.js
@@ -13,15 +13,22 @@ client.on("ready", function()
 
 client.on('message', function(msg)
 {
-  if(msg.content.toUpperCase().startsWith("STATECHANGER "))
+  if(msg.content.toUpperCase().startsWith("STATECHANGER CHANGE "))
   {
     if(msg.author.id == client.user.id) //if the message is from the selfbot user
     {
-      var param_phrase = msg.content.slice(15); //cut out the prefix + the 2 spaces + the type number
-      var param_type = msg.content.charAt(13);// get the 14th char (prefix + 1 space)
-      var options = {type: Number(param_type)}//Set the option for the setActivity
-      client.user.setActivity(param_phrase,options);//set the activity
-      console.log("State changed to state " + param_type + " with the name of game '" + param_phrase + "'");//Keep a trace of what you
+      var param_phrase = msg.content.slice(22); //cut out the prefix + the 2 spaces + the type number
+      var param_type = msg.content.charAt(20); // get the 14th char (prefix + 1 space)
+      var options = {type: Number(param_type)} //Set the option for the setActivity
+      client.user.setActivity(param_phrase,options); //set the activity
+      console.log("State changed to state " + param_type + " with the name of game '" + param_phrase + "'"); //Keep a trace of what you
+    }
+  }
+  if(msg.content.toUpperCase().startsWith("STATECHANGER STATUS"))
+  {
+    if(msg.author.presence.game !== null)
+    {
+      console.log(msg.author + " is doing type " + msg.author.presence.game.type + " on the game '" + msg.author.presence.game.name + "'");
     }
   }
 });
