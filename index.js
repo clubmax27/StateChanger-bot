62f364615cc2ae873b04be48b88fad040196495c
clubmax27 <Glados.Plague@gmail.com>
2018-01-21 01:28:56 +0100

    Added a security test
    Added a security test to check if the message author is the selfbot user

--- index.js
+++ index.js
@@ -15,10 +15,13 @@ client.on('message', function(msg)
 {
   if(msg.content.toUpperCase().startsWith("STATECHANGER "))
   {
-    var param_phrase = msg.content.slice(15); //cut out the prefix + the 2 spaces + the type number
-    var param_type = msg.content.charAt(13);// get the 14th char (prefix + 1 space)
-    var options = {type: Number(param_type)}//Set the option for the setActivity
-    client.user.setActivity(param_phrase,options);//set the activity
-    console.log("State changed to state " + param_type + " with the name of game '" + param_phrase + "'");//Keep a trace of what you
+    if(msg.author.id == client.user.id) //if the message is from the selfbot user
+    {
+      var param_phrase = msg.content.slice(15); //cut out the prefix + the 2 spaces + the type number
+      var param_type = msg.content.charAt(13);// get the 14th char (prefix + 1 space)
+      var options = {type: Number(param_type)}//Set the option for the setActivity
+      client.user.setActivity(param_phrase,options);//set the activity
+      console.log("State changed to state " + param_type + " with the name of game '" + param_phrase + "'");//Keep a trace of what you
+    }
   }
 });
