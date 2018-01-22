042ef70b2ca99481fc0d4976dd84c9fbead3335e
clubmax27 <Glados.Plague@gmail.com>
2018-01-22 21:56:57 +0100

    Added useless semicolums

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
@@ -6,7 +8,7 @@ client.login("YOUR_TOKEN");
 
 client.on("ready", function()
 {
-  var options = {type: 0}
+  var options = {type: 0};
   client.user.setActivity("",options); //setting the activity to null when the selfbot start
   console.log("Logged in");
 });
@@ -19,7 +21,7 @@ client.on('message', function(msg)
     {
       var param_phrase = msg.content.slice(22); //cut out the prefix + the 2 spaces + the type number
       var param_type = msg.content.charAt(20); // get the 14th char (prefix + 1 space)
-      var options = {type: Number(param_type)} //Set the option for the setActivity
+      var options = {type: Number(param_type)}; //Set the option for the setActivity
       client.user.setActivity(param_phrase,options); //set the activity
       console.log("State changed to state " + param_type + " with the name of game '" + param_phrase + "'"); //Keep a trace of what you
     }
